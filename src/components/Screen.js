c287b4a6889d91041d7c901351f10dc0721b075a
elhicks01 <emilylhicks01@gmail.com>
2024-06-19 18:22:07 -0400

    added project 1

---
+++ src/components/Screen.js
@@ -0,0 +1,13 @@
+// src/components/Screen.js
+import React from 'react';
+import "./Screen.css";
+
+const Screen = ({ value }) => {
+  return (
+    <div className="screen">
+      {value}
+    </div>
+  );
+};
+
+export default Screen;
